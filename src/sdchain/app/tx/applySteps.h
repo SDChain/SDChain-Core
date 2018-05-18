dd3b9b4b318af9e0754a8c35f57b957c4d119a93
makeupforever <xwb2766@126.com>
2018-05-18 18:18:42 +0800

    add transactions module

---
+++ src/sdchain/app/tx/applySteps.h
@@ -0,0 +1,276 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of sdchaind: https://github.com/SDChain/SDChain-core
+    Copyright (c) 2017, 2018 SDChain Alliance.
+
+
+
+*/
+//==============================================================================
+
+#ifndef SDCHAIN_TX_APPLYSTEPS_H_INCLUDED
+#define SDCHAIN_TX_APPLYSTEPS_H_INCLUDED
+
+#include <sdchain/ledger/ApplyViewImpl.h>
+#include <sdchain/beast/utility/Journal.h>
+
+namespace sdchain {
+
+class Application;
+class STTx;
+
+/** Describes the results of the `preflight` check
+
+    @note All members are const to make it more difficult
+        to "fake" a result without calling `preflight`.
+    @see preflight, preclaim, doApply, apply
+*/
+struct PreflightResult
+{
+public:
+    /// From the input - the transaction
+    STTx const& tx;
+    /// From the input - the rules
+    Rules const rules;
+    /// From the input - the flags
+    ApplyFlags const flags;
+    /// From the input - the journal
+    beast::Journal const j;
+
+    /// Intermediate transaction result
+    TER const ter;
+
+    /// Constructor
+    template<class Context>
+    PreflightResult(Context const& ctx_,
+        TER ter_)
+        : tx(ctx_.tx)
+        , rules(ctx_.rules)
+        , flags(ctx_.flags)
+        , j(ctx_.j)
+        , ter(ter_)
+    {
+    }
+
+    PreflightResult(PreflightResult const&) = default;
+    /// Deleted copy assignment operator
+    PreflightResult& operator=(PreflightResult const&) = delete;
+};
+
+/** Describes the results of the `preclaim` check
+
+    @note All members are const to make it more difficult
+        to "fake" a result without calling `preclaim`.
+    @see preflight, preclaim, doApply, apply
+*/
+struct PreclaimResult
+{
+public:
+    /// From the input - the ledger view
+    ReadView const& view;
+    /// From the input - the transaction
+    STTx const& tx;
+    /// From the input - the flags
+    ApplyFlags const flags;
+    /// From the input - the journal
+    beast::Journal const j;
+
+    /// Intermediate transaction result
+    TER const ter;
+    /// Transaction-specific base fee
+    std::uint64_t const baseFee;
+    /// Success flag - whether the transaction is likely to
+    /// claim a fee
+    bool const likelyToClaimFee;
+
+    /// Constructor
+    template<class Context>
+    PreclaimResult(Context const& ctx_,
+        TER ter_, std::uint64_t const& baseFee_)
+        : view(ctx_.view)
+        , tx(ctx_.tx)
+        , flags(ctx_.flags)
+        , j(ctx_.j)
+        , ter(ter_)
+        , baseFee(baseFee_)
+        , likelyToClaimFee(ter == tesSUCCESS
+            || isTecClaim(ter))
+    {
+    }
+
+    /// Constructor
+    template<class Context>
+    PreclaimResult(Context const& ctx_,
+        std::pair<TER, std::uint64_t> const& result)
+        : PreclaimResult(ctx_, result.first, result.second)
+    {
+    }
+
+    PreclaimResult(PreclaimResult const&) = default;
+    /// Deleted copy assignment operator
+    PreclaimResult& operator=(PreclaimResult const&) = delete;
+};
+
+/** Structure describing the consequences to the account
+    of applying a transaction if the transaction consumes
+    the maximum SDA allowed.
+
+    @see calculateConsequences
+*/
+struct TxConsequences
+{
+    /// Describes how the transaction affects subsequent
+    /// transactions
+    enum ConsequenceCategory
+    {
+        /// Moves currency around, creates offers, etc.
+        normal = 0,
+        /// Affects the ability of subsequent transactions
+        /// to claim a fee. Eg. `SetRegularKey`
+        blocker
+    };
+
+    /// Describes how the transaction affects subsequent
+    /// transactions
+    ConsequenceCategory const category;
+    /// Transaction fee
+    SDAAmount const fee;
+    /// Does NOT include the fee.
+    SDAAmount const potentialSpend;
+
+    /// Constructor
+    TxConsequences(ConsequenceCategory const category_,
+        SDAAmount const fee_, SDAAmount const spend_)
+        : category(category_)
+        , fee(fee_)
+        , potentialSpend(spend_)
+    {
+    }
+
+    /// Constructor
+    TxConsequences(TxConsequences const&) = default;
+    /// Deleted copy assignment operator
+    TxConsequences& operator=(TxConsequences const&) = delete;
+    /// Constructor
+    TxConsequences(TxConsequences&&) = default;
+    /// Deleted copy assignment operator
+    TxConsequences& operator=(TxConsequences&&) = delete;
+
+};
+
+/** Gate a transaction based on static information.
+
+    The transaction is checked against all possible
+    validity constraints that do not require a ledger.
+
+    @param app The current running `Application`.
+    @param rules The `Rules` in effect at the time of the check.
+    @param tx The transaction to be checked.
+    @param flags `ApplyFlags` describing processing options.
+    @param j A journal.
+
+    @see PreflightResult, preclaim, doApply, apply
+
+    @return A `PreflightResult` object containing, among
+    other things, the `TER` code.
+*/
+PreflightResult
+preflight(Application& app, Rules const& rules,
+    STTx const& tx, ApplyFlags flags,
+        beast::Journal j);
+
+/** Gate a transaction based on static ledger information.
+
+    The transaction is checked against all possible
+    validity constraints that DO require a ledger.
+
+    If preclaim succeeds, then the transaction is very
+    likely to claim a fee. This will determine if the
+    transaction is safe to relay without being applied
+    to the open ledger.
+
+    "Succeeds" in this case is defined as returning a
+    `tes` or `tec`, since both lead to claiming a fee.
+
+    @pre The transaction has been checked
+    and validated using `preflight`
+
+    @param preflightResult The result of a previous
+        call to `preflight` for the transaction.
+    @param app The current running `Application`.
+    @param view The open ledger that the transaction
+        will attempt to be applied to.
+
+    @see PreclaimResult, preflight, doApply, apply
+
+    @return A `PreclaimResult` object containing, among
+    other things the `TER` code and the base fee value for
+    this transaction.
+*/
+PreclaimResult
+preclaim(PreflightResult const& preflightResult,
+    Application& app, OpenView const& view);
+
+/** Compute only the expected base fee for a transaction.
+
+    Base fees are transaction specific, so any calculation
+    needing them must get the base fee for each transaction.
+
+    No validation is done or implied by this function.
+
+    Caller is responsible for handling any exceptions.
+    Since none should be thrown, that will usually
+    mean terminating.
+
+    @param app The current running `Application`.
+    @param view The current open ledger.
+    @param tx The transaction to be checked.
+    @param j A journal.
+
+    @return The base fee.
+*/
+std::uint64_t
+calculateBaseFee(Application& app, ReadView const& view,
+    STTx const& tx, beast::Journal j);
+
+/** Determine the SDA balance consequences if a transaction
+    consumes the maximum SDA allowed.
+
+    @pre The transaction has been checked
+    and validated using `preflight`
+
+    @param preflightResult The result of a previous
+    call to `preflight` for the transaction.
+
+    @return A `TxConsequences` object containing the "worst
+        case" consequences of applying this transaction to
+        a ledger.
+
+    @see TxConsequences
+*/
+TxConsequences
+calculateConsequences(PreflightResult const& preflightResult);
+
+/** Apply a prechecked transaction to an OpenView.
+
+    @pre The transaction has been checked
+    and validated using `preflight` and `preclaim`
+
+    @param preclaimResult The result of a previous
+    call to `preclaim` for the transaction.
+    @param app The current running `Application`.
+    @param view The open ledger that the transaction
+    will attempt to be applied to.
+
+    @see preflight, preclaim, apply
+
+    @return A pair with the `TER` and a `bool` indicating
+    whether or not the transaction was applied.
+*/
+std::pair<TER, bool>
+doApply(PreclaimResult const& preclaimResult,
+    Application& app, OpenView& view);
+
+}
+
+#endif
---
+++ src/sdchain/app/tx/impl/applySteps.cpp
@@ -0,0 +1,327 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of sdchaind: https://github.com/SDChain/SDChain-core
+    Copyright (c) 2017, 2018 SDChain Alliance.
+
+
+
+*/
+//==============================================================================
+
+#include <BeastConfig.h>
+#include <sdchain/app/tx/applySteps.h>
+#include <sdchain/app/tx/impl/ApplyContext.h>
+#include <sdchain/app/tx/impl/CancelOffer.h>
+#include <sdchain/app/tx/impl/CancelTicket.h>
+#include <sdchain/app/tx/impl/Change.h>
+#include <sdchain/app/tx/impl/CreateOffer.h>
+#include <sdchain/app/tx/impl/CreateTicket.h>
+#include <sdchain/app/tx/impl/Escrow.h>
+#include <sdchain/app/tx/impl/Payment.h>
+#include <sdchain/app/tx/impl/SetAccount.h>
+#include <sdchain/app/tx/impl/SetRegularKey.h>
+#include <sdchain/app/tx/impl/SetSignerList.h>
+//#include <sdchain/app/tx/impl/SetTrust.h>
+#include <sdchain/app/tx/impl/PayChan.h>
+
+namespace sdchain {
+
+static
+TER
+invoke_preflight (PreflightContext const& ctx)
+{
+    switch(ctx.tx.getTxnType())
+    {
+    case ttACCOUNT_SET:     return SetAccount       ::preflight(ctx);
+    case ttOFFER_CANCEL:    return CancelOffer      ::preflight(ctx);
+    case ttOFFER_CREATE:    return CreateOffer      ::preflight(ctx);
+    case ttPAYMENT:         return Payment          ::preflight(ctx);
+    case ttESCROW_CREATE:   return EscrowCreate     ::preflight(ctx);
+    case ttESCROW_FINISH:   return EscrowFinish     ::preflight(ctx);
+    case ttESCROW_CANCEL:   return EscrowCancel     ::preflight(ctx);
+    case ttREGULAR_KEY_SET: return SetRegularKey    ::preflight(ctx);
+    case ttSIGNER_LIST_SET: return SetSignerList    ::preflight(ctx);
+    case ttTICKET_CANCEL:   return CancelTicket     ::preflight(ctx);
+    case ttTICKET_CREATE:   return CreateTicket     ::preflight(ctx);
+//    case ttTRUST_SET:       return SetTrust         ::preflight(ctx);
+    case ttAMENDMENT:
+    case ttFEE:             return Change           ::preflight(ctx);
+    case ttPAYCHAN_CREATE:  return PayChanCreate    ::preflight(ctx);
+    case ttPAYCHAN_FUND:    return PayChanFund      ::preflight(ctx);
+    case ttPAYCHAN_CLAIM:   return PayChanClaim     ::preflight(ctx);
+    default:
+        assert(false);
+        return temUNKNOWN;
+    }
+}
+
+/* invoke_preclaim<T> uses name hiding to accomplish
+    compile-time polymorphism of (presumably) static
+    class functions for Transactor and derived classes.
+*/
+template<class T>
+static
+std::pair<TER, std::uint64_t>
+invoke_preclaim(PreclaimContext const& ctx)
+{
+    // If the transactor requires a valid account and the transaction doesn't
+    // list one, preflight will have already a flagged a failure.
+    auto const id = ctx.tx.getAccountID(sfAccount);
+    auto const baseFee = T::calculateBaseFee(ctx);
+
+    if (id != zero)
+    {
+        TER result = T::checkSeq(ctx);
+
+        if (result != tesSUCCESS)
+            return { result, baseFee };
+
+        result = T::checkFee(ctx, baseFee);
+
+        if (result != tesSUCCESS)
+            return { result, baseFee };
+
+        result = T::checkSign(ctx);
+
+        if (result != tesSUCCESS)
+            return { result, baseFee };
+
+    }
+
+    return{ T::preclaim(ctx), baseFee };
+}
+
+static
+std::pair<TER, std::uint64_t>
+invoke_preclaim (PreclaimContext const& ctx)
+{
+    switch(ctx.tx.getTxnType())
+    {
+    case ttACCOUNT_SET:     return invoke_preclaim<SetAccount>(ctx);
+    case ttOFFER_CANCEL:    return invoke_preclaim<CancelOffer>(ctx);
+    case ttOFFER_CREATE:    return invoke_preclaim<CreateOffer>(ctx);
+    case ttPAYMENT:         return invoke_preclaim<Payment>(ctx);
+    case ttESCROW_CREATE:   return invoke_preclaim<EscrowCreate>(ctx);
+    case ttESCROW_FINISH:   return invoke_preclaim<EscrowFinish>(ctx);
+    case ttESCROW_CANCEL:   return invoke_preclaim<EscrowCancel>(ctx);
+    case ttREGULAR_KEY_SET: return invoke_preclaim<SetRegularKey>(ctx);
+    case ttSIGNER_LIST_SET: return invoke_preclaim<SetSignerList>(ctx);
+    case ttTICKET_CANCEL:   return invoke_preclaim<CancelTicket>(ctx);
+    case ttTICKET_CREATE:   return invoke_preclaim<CreateTicket>(ctx);
+//    case ttTRUST_SET:       return invoke_preclaim<SetTrust>(ctx);
+    case ttAMENDMENT:
+    case ttFEE:             return invoke_preclaim<Change>(ctx);
+    case ttPAYCHAN_CREATE:  return invoke_preclaim<PayChanCreate>(ctx);
+    case ttPAYCHAN_FUND:    return invoke_preclaim<PayChanFund>(ctx);
+    case ttPAYCHAN_CLAIM:   return invoke_preclaim<PayChanClaim>(ctx);
+    default:
+        assert(false);
+        return { temUNKNOWN, 0 };
+    }
+}
+
+static
+std::uint64_t
+invoke_calculateBaseFee(PreclaimContext const& ctx)
+{
+    switch (ctx.tx.getTxnType())
+    {
+    case ttACCOUNT_SET:     return SetAccount::calculateBaseFee(ctx);
+    case ttOFFER_CANCEL:    return CancelOffer::calculateBaseFee(ctx);
+    case ttOFFER_CREATE:    return CreateOffer::calculateBaseFee(ctx);
+    case ttPAYMENT:         return Payment::calculateBaseFee(ctx);
+    case ttESCROW_CREATE:   return EscrowCreate::calculateBaseFee(ctx);
+    case ttESCROW_FINISH:   return EscrowFinish::calculateBaseFee(ctx);
+    case ttESCROW_CANCEL:   return EscrowCancel::calculateBaseFee(ctx);
+    case ttREGULAR_KEY_SET: return SetRegularKey::calculateBaseFee(ctx);
+    case ttSIGNER_LIST_SET: return SetSignerList::calculateBaseFee(ctx);
+    case ttTICKET_CANCEL:   return CancelTicket::calculateBaseFee(ctx);
+    case ttTICKET_CREATE:   return CreateTicket::calculateBaseFee(ctx);
+//    case ttTRUST_SET:       return SetTrust::calculateBaseFee(ctx);
+    case ttAMENDMENT:
+    case ttFEE:             return Change::calculateBaseFee(ctx);
+    case ttPAYCHAN_CREATE:  return PayChanCreate::calculateBaseFee(ctx);
+    case ttPAYCHAN_FUND:    return PayChanFund::calculateBaseFee(ctx);
+    case ttPAYCHAN_CLAIM:   return PayChanClaim::calculateBaseFee(ctx);
+    default:
+        assert(false);
+        return 0;
+    }
+}
+
+template<class T>
+static
+TxConsequences
+invoke_calculateConsequences(STTx const& tx)
+{
+    auto const category = T::affectsSubsequentTransactionAuth(tx) ?
+        TxConsequences::blocker : TxConsequences::normal;
+    auto const feePaid = T::calculateFeePaid(tx);
+    auto const maxSpend = T::calculateMaxSpend(tx);
+
+    return{ category, feePaid, maxSpend };
+}
+
+static
+TxConsequences
+invoke_calculateConsequences(STTx const& tx)
+{
+    switch (tx.getTxnType())
+    {
+    case ttACCOUNT_SET:     return invoke_calculateConsequences<SetAccount>(tx);
+    case ttOFFER_CANCEL:    return invoke_calculateConsequences<CancelOffer>(tx);
+    case ttOFFER_CREATE:    return invoke_calculateConsequences<CreateOffer>(tx);
+    case ttPAYMENT:         return invoke_calculateConsequences<Payment>(tx);
+    case ttESCROW_CREATE:   return invoke_calculateConsequences<EscrowCreate>(tx);
+    case ttESCROW_FINISH:   return invoke_calculateConsequences<EscrowFinish>(tx);
+    case ttESCROW_CANCEL:   return invoke_calculateConsequences<EscrowCancel>(tx);
+    case ttREGULAR_KEY_SET: return invoke_calculateConsequences<SetRegularKey>(tx);
+    case ttSIGNER_LIST_SET: return invoke_calculateConsequences<SetSignerList>(tx);
+    case ttTICKET_CANCEL:   return invoke_calculateConsequences<CancelTicket>(tx);
+    case ttTICKET_CREATE:   return invoke_calculateConsequences<CreateTicket>(tx);
+//    case ttTRUST_SET:       return invoke_calculateConsequences<SetTrust>(tx);
+    case ttPAYCHAN_CREATE:  return invoke_calculateConsequences<PayChanCreate>(tx);
+    case ttPAYCHAN_FUND:    return invoke_calculateConsequences<PayChanFund>(tx);
+    case ttPAYCHAN_CLAIM:   return invoke_calculateConsequences<PayChanClaim>(tx);
+    case ttAMENDMENT:
+    case ttFEE:
+        // fall through to default
+    default:
+        assert(false);
+        return { TxConsequences::blocker, Transactor::calculateFeePaid(tx),
+            beast::zero };
+    }
+}
+
+static
+std::pair<TER, bool>
+invoke_apply (ApplyContext& ctx)
+{
+    switch(ctx.tx.getTxnType())
+    {
+    case ttACCOUNT_SET:     { SetAccount    p(ctx); return p(); }
+    case ttOFFER_CANCEL:    { CancelOffer   p(ctx); return p(); }
+    case ttOFFER_CREATE:    { CreateOffer   p(ctx); return p(); }
+    case ttPAYMENT:         { Payment       p(ctx); return p(); }
+    case ttESCROW_CREATE:   { EscrowCreate  p(ctx); return p(); }
+    case ttESCROW_FINISH:   { EscrowFinish  p(ctx); return p(); }
+    case ttESCROW_CANCEL:   { EscrowCancel  p(ctx); return p(); }
+    case ttREGULAR_KEY_SET: { SetRegularKey p(ctx); return p(); }
+    case ttSIGNER_LIST_SET: { SetSignerList p(ctx); return p(); }
+    case ttTICKET_CANCEL:   { CancelTicket  p(ctx); return p(); }
+    case ttTICKET_CREATE:   { CreateTicket  p(ctx); return p(); }
+//    case ttTRUST_SET:       { SetTrust      p(ctx); return p(); }
+    case ttAMENDMENT:
+    case ttFEE:             { Change        p(ctx); return p(); }
+    case ttPAYCHAN_CREATE:  { PayChanCreate p(ctx); return p(); }
+    case ttPAYCHAN_FUND:    { PayChanFund   p(ctx); return p(); }
+    case ttPAYCHAN_CLAIM:   { PayChanClaim  p(ctx); return p(); }
+    default:
+        assert(false);
+        return { temUNKNOWN, false };
+    }
+}
+
+PreflightResult
+preflight(Application& app, Rules const& rules,
+    STTx const& tx, ApplyFlags flags,
+        beast::Journal j)
+{
+    PreflightContext const pfctx(app, tx,
+        rules, flags, j);
+    try
+    {
+        return{ pfctx, invoke_preflight(pfctx) };
+    }
+    catch (std::exception const& e)
+    {
+        JLOG(j.fatal()) <<
+            "apply: " << e.what();
+        return{ pfctx, tefEXCEPTION };
+    }
+}
+
+PreclaimResult
+preclaim (PreflightResult const& preflightResult,
+    Application& app, OpenView const& view)
+{
+    boost::optional<PreclaimContext const> ctx;
+    if (preflightResult.rules != view.rules())
+    {
+        auto secondFlight = preflight(app, view.rules(),
+            preflightResult.tx, preflightResult.flags,
+                preflightResult.j);
+        ctx.emplace(app, view, secondFlight.ter, secondFlight.tx,
+            secondFlight.flags, secondFlight.j);
+    }
+    else
+    {
+        ctx.emplace(
+            app, view, preflightResult.ter, preflightResult.tx,
+                preflightResult.flags, preflightResult.j);
+    }
+    try
+    {
+        if (ctx->preflightResult != tesSUCCESS)
+            return { *ctx, ctx->preflightResult, 0 };
+        return{ *ctx, invoke_preclaim(*ctx) };
+    }
+    catch (std::exception const& e)
+    {
+        JLOG(ctx->j.fatal()) <<
+            "apply: " << e.what();
+        return{ *ctx, tefEXCEPTION, 0 };
+    }
+}
+
+std::uint64_t
+calculateBaseFee(Application& app, ReadView const& view,
+    STTx const& tx, beast::Journal j)
+{
+    PreclaimContext const ctx(
+        app, view, tesSUCCESS, tx,
+            tapNONE, j);
+
+    return invoke_calculateBaseFee(ctx);
+}
+
+TxConsequences
+calculateConsequences(PreflightResult const& preflightResult)
+{
+    assert(preflightResult.ter == tesSUCCESS);
+    if (preflightResult.ter != tesSUCCESS)
+        return{ TxConsequences::blocker,
+            Transactor::calculateFeePaid(preflightResult.tx),
+                beast::zero };
+    return invoke_calculateConsequences(preflightResult.tx);
+}
+
+std::pair<TER, bool>
+doApply(PreclaimResult const& preclaimResult,
+    Application& app, OpenView& view)
+{
+    if (preclaimResult.view.seq() != view.seq())
+    {
+        // Logic error from the caller. Don't have enough
+        // info to recover.
+        return{ tefEXCEPTION, false };
+    }
+    try
+    {
+        if (!preclaimResult.likelyToClaimFee)
+            return{ preclaimResult.ter, false };
+        ApplyContext ctx(app, view,
+            preclaimResult.tx, preclaimResult.ter,
+                preclaimResult.baseFee, preclaimResult.flags,
+                    preclaimResult.j);
+        return invoke_apply(ctx);
+    }
+    catch (std::exception const& e)
+    {
+        JLOG(preclaimResult.j.fatal()) <<
+            "apply: " << e.what();
+        return { tefEXCEPTION, false };
+    }
+}
+
+} // sdchain
