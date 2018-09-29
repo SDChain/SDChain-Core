a0c1d69105d76cb6b9fcb75389ea1f2a9f8349a9
MakeupForever <xwb2766@126.com>
2018-09-29 18:30:05 +0800

    add sdchaind tuning

---
+++ src/sdchain/app/main/Tuning.h
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+/*
+    This file is part of sdchaind: https://github.com/SDChain/SDChain-core
+    Copyright (c) 2017, 2018 SDChain Alliance.
+
+
+
+*/
+//==============================================================================
+
+#ifndef SDCHAIN_APP_MAIN_TUNING_H_INCLUDED
+#define SDCHAIN_APP_MAIN_TUNING_H_INCLUDED
+
+namespace sdchain {
+
+enum
+{
+     fullBelowTargetSize = 524288
+    ,fullBelowExpirationSeconds = 600
+};
+
+}
+
+#endif
